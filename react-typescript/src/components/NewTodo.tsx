a5a7ed2ff0ca202fe8874dba2bef7b2ecd4712f5
Aristela Marku <aristela.marku@gmail.com>
2022-02-23 21:30:07 -0500

    css styling

--- react-typescript/src/components/NewTodo.tsx
+++ react-typescript/src/components/NewTodo.tsx
@@ -1,28 +1,30 @@
-import React from "react"
-import { useRef } from "react"
+import { useRef } from 'react';
 
-const NewTodo: React.FC<{onAddTodo: (text:string) => void}> = (props) =>{
+import classes from './NewTodo.module.css';
 
-    const todoTextInputRef = useRef<HTMLInputElement>(null);
-   
-    const submitHandler = (event: React.FormEvent) => {
-    event.preventDefault()
-    const enteredText = todoTextInputRef.current!.value
-    if(enteredText.trim().length === 0){
-        return;
+const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
+  const todoTextInputRef = useRef<HTMLInputElement>(null);
+
+  const submitHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    const enteredText = todoTextInputRef.current!.value;
+
+    if (enteredText.trim().length === 0) {
+      // throw an error
+      return;
     }
-    props.onAddTodo(enteredText)
-}
 
+    props.onAddTodo(enteredText);
+  };
 
-   return (
-       <form onSubmit={submitHandler}>
-           <label htmlFor='text'>Todo Text</label>
-           <input type='text' ref={todoTextInputRef}/>
-           <button>Add Todo</button>
-       </form>
-   )
+  return (
+    <form onSubmit={submitHandler} className={classes.form}>
+      <label htmlFor='text'>Todo text</label>
+      <input type='text' id='text' ref={todoTextInputRef} />
+      <button>Add Todo</button>
+    </form>
+  );
+};
 
-}
-
-export default NewTodo
+export default NewTodo;
